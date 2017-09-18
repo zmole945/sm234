d3298e2ce3771045a9d22d2ab83edfd9a3450664
shiz <zmole945@163.com>
2017-09-18 17:41:22 +0800

    add msg digest

--- SM3/sm3test.c
+++ SM3/sm3test.c
@@ -3,43 +3,68 @@
 #include <stdio.h>
 #include "sm3.h"
 
+static void test_msg(void);
+
 int main( int argc, char *argv[] )
 {
-	unsigned char *input = "abc";
-	int ilen = 3;
-	unsigned char output[32];
-	int i;
-	sm3_context ctx;
+    unsigned char *input = "abc";
+    int ilen = 3;
+    unsigned char output[32];
+    int i;
+    sm3_context ctx;
 
-	printf("Message:\n");
-	printf("%s\n",input);
+    printf("Message:\n");
+    printf("%s\n",input);
 
-	sm3(input, ilen, output);
-	printf("Hash:\n   ");
-	for(i=0; i<32; i++)
-	{
-		printf("%02x",output[i]);
-		if (((i+1) % 4 ) == 0) printf(" ");
-	} 
-	printf("\n");
+    sm3(input, ilen, output);
+    printf("Hash:\n   ");
+    for(i=0; i<32; i++)
+    {
+        printf("%02x",output[i]);
+        if (((i+1) % 4 ) == 0) printf(" ");
+    }
+    printf("\n");
 
-	printf("Message:\n");
-	for(i=0; i < 16; i++)
-		printf("abcd");
-	printf("\n");
+    printf("Message:\n");
+    for(i=0; i < 16; i++)
+        printf("abcd");
+    printf("\n");
 
     sm3_starts( &ctx );
-	for(i=0; i < 16; i++)
-		sm3_update( &ctx, "abcd", 4 );
+    for(i=0; i < 16; i++)
+        sm3_update( &ctx, "abcd", 4 );
     sm3_finish( &ctx, output );
     memset( &ctx, 0, sizeof( sm3_context ) );
-	
-	printf("Hash:\n   ");
-	for(i=0; i<32; i++)
-	{
-		printf("%02x",output[i]);
-		if (((i+1) % 4 ) == 0) printf(" ");
-	}   
-	printf("\n");
-    //getch();	//VS2008 
+
+    printf("Hash:\n   ");
+    for(i=0; i<32; i++)
+    {
+        printf("%02x",output[i]);
+        if (((i+1) % 4 ) == 0) printf(" ");
+    }
+    printf("\n");
+
+    test_msg();
+
+    //getch();	//VS2008
 }
+
+static void test_msg(void)
+{
+    unsigned char msg[] = "message digest";
+    unsigned char output[32];
+    int i;
+
+    printf("Message:\n");
+    printf("%s\n", msg);
+
+    sm3(msg, strlen(msg), output);
+    printf("Hash:\n   ");
+    for(i=0; i<32; i++)
+    {
+        printf("%02x",output[i]);
+        if (((i+1) % 4 ) == 0) printf(" ");
+    }
+    printf("\n");
+}
+
